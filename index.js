6fdccb5996e21b81e829335dfaaba295dd8ade4f
ElnurBX <tu8dhdtwy@code.edu.az>
2024-04-03 00:16:43 +0400

    final

---
+++ index.js
@@ -0,0 +1,41 @@
+// // task 1
+// let eded1 =parseInt(prompt("eded daxil edin"))
+// let eded2 =parseInt(prompt("eded daxil edin"))
+// let eded3 =parseInt(prompt("eded daxil edin"))
+
+// let ededi_orta=(eded1+eded2+eded3)/3
+// console.log(ededi_orta);
+
+// //task 2
+// let teref1=parseInt(prompt("eded daxil edin"))
+// let teref2=parseInt(prompt("eded daxil edin"))
+// let teref3=parseInt(prompt("eded daxil edin"))
+
+// if (teref1 + teref2 > teref3 && teref1 + teref3 > teref2 && teref2 + teref3 > teref1) {
+//     console.log(true)
+// }else{
+//     console.log(false);
+// }
+
+// // task3
+// let name = prompt('name')
+// let surname = prompt('surname')
+// let fullname= name+" "+surname
+// console.log(fullname);
+
+
+// // task 4
+// let  boy = parseFloat(prompt("boyunuzu daxil edin (metir ilə)"))
+// let  cəki =parseFloat(prompt("çəkinizi daxil edin  (kq ilə)"))
+// bmi=cəki/(boy*boy)
+// if(0>bmi ){
+//     console.log("daxil etdiyiniz reqemler munkun ola bilmez");
+// }else if(18.5>bmi){
+//     console.log("arıq");
+// }else if(18.5<bmi && bmi <24.9){
+//     console.log(" normal ");
+// }else if(bmi>25){
+//     console.log("artıq çəkili",bmi );
+// }else{
+//     console.log("ədəd daxil etmediniz",bmi);
+// }
---
+++ silinen_tasklar.js
@@ -0,0 +1,69 @@
+// task1 
+// let a =parseInt(prompt('eded daxil et'))
+
+// if( 0>a  ){
+//     console.log("nə sadə nədə murekebdir ededir");
+// }else if(a===1 || a===2 || a===3){
+//     console.log("sade ededdir");
+// }else if(--a % 6 == 0){
+//     console.log("sade ededdir");
+// }else{
+//     console.log("murekeb ededir");
+// }
+
+// // fabonachi taski 
+// let fab1=parseInt(prompt("Fibonacci ədədləri"));
+// let fab2=parseInt(prompt("Fibonacci ədədləri"));
+// let fab3=parseInt(prompt("Fibonacci ədədləri"));
+// let number
+
+// number=fab1
+// let temp1 = 5 * number * number + 4;
+// let temp2 = 5 * number * number - 4;
+// let sqrtTemp1 = parseInt((temp1)**0.5);
+// let sqrtTemp2 = parseInt((temp2)**0.5);
+// if((sqrtTemp1 * sqrtTemp1 === temp1) || (sqrtTemp2 * sqrtTemp2 === temp2)){
+//     fibonacci_value1=true;
+// }else{
+//     fibonacci_value1=false 
+// };
+
+// number=fab2
+// temp1 = 5 * number * number + 4;
+// temp2 = 5 * number * number - 4;
+// sqrtTemp1 = parseInt((temp1)**0.5);
+// sqrtTemp2 = parseInt((temp2)**0.5);
+// if((sqrtTemp1 * sqrtTemp1 === temp1) || (sqrtTemp2 * sqrtTemp2 === temp2)){
+//     fibonacci_value2=true;
+// }else{
+//     fibonacci_value2=false  
+// };
+
+
+// number=fab3
+// temp1 = 5 * number * number + 4;
+// temp2 = 5 * number * number - 4;
+// sqrtTemp1 = parseInt((temp1)**0.5);
+// sqrtTemp2 = parseInt((temp2)**0.5);
+// if((sqrtTemp1 * sqrtTemp1 === temp1) || (sqrtTemp2 * sqrtTemp2 === temp2)){
+//     fibonacci_value3=true;
+// }else{
+//     fibonacci_value3=false 
+// };
+
+
+
+// //tələblərə uygun
+// if(fibonacci_value1==fibonacci_value2 && fibonacci_value3==fibonacci_value2){
+//     console.log(true)
+// }else if(fibonacci_value1==fibonacci_value2 || fibonacci_value3==fibonacci_value2){
+//     console.log(false);
+// }
+// // daha yaxşi oldugunu dusunduyum
+// // if(fibonacci_value1==fibonacci_value2 && fibonacci_value3==fibonacci_value2){
+// //     console.log(fab1,",",fab2,",",fab3,"  ədədlərinin hamisi fibonaççi ədələridir")
+// // }else if(fibonacci_value1==fibonacci_value2 || fibonacci_value3==fibonacci_value2){
+// //     console.log("ededlerden bezileri fibonaççi  ededidir");
+// // }else{
+// //     console.log("hec biri fibonaççi ededi deyil");
+// // }
